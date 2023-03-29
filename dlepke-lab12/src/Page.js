7f4751696d000de065853d47035cdb28aa520637
dlepke <deannalepke@gmail.com>
2023-03-28 22:34:27 -0700

    functionality complete

---
+++ dlepke-lab12/src/Page.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Pokemon from "./Pokemon";
+function page({ currentPokemons, currentPage }) {
+  return (
+    <div>
+      <h1>Page number {currentPage}</h1>
+      <div className="pokemon-list">
+        {currentPokemons.map((item) => {
+          return <Pokemon pokemon={item} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default page;
